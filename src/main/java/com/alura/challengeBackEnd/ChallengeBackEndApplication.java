8c9e348116c92da8e9912391a0124dafb02fad9a
julianomachadoo <julmachadoramos@gmail.com>
2022-11-01 15:25:39 -0300

    Projeto iniciado, banco previamente escolhido : mongoDB

---
+++ src/main/java/com/alura/challengeBackEnd/ChallengeBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.alura.challengeBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/challengeBackEnd/ChallengeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.challengeBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
